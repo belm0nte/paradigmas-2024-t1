a66e0d1c2f889d2fe54b631cab4b0638416ae593
belm0nte <arthurbelmonte2004@gmail.com>
2024-04-24 07:12:18 +0000

    atualização trabalho

--- src/lista.c
+++ src/lista.c
@@ -3,4 +3,34 @@
  */
 
 #include "lista.h"
+#include "stdio.h"
+#include "string.h"
 
+
+typedef struct node {
+	void* data;
+
+	struct node* next;
+}
+typedef struct Lista {
+	int data_size;
+	node* head;
+	void (*free_data)(void*)
+}
+
+void Lista_new(Lista* lista, int data_size, void (*free_data) (void*)){
+	lista->data_size = data_size;
+	lista->head = NULL;
+	lista->free_data = free_data;
+	
+}
+
+void  Lista_delete(Lista* lista){
+	Node* current = lista->head;
+	while (current != NULL){ 
+	Node* next = current->next;
+	lista->free_data(current->data);
+	free(current);
+
+	
+}
